6b321ead6a88f0a219a981f7499c6e6647313ab9
Mainorrr <mainor.cas.v@gmail.com>
2026-05-17 14:10:54 -0600

    Migrar a createBrowserRouter con basename para corregir rutas en GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Home from './pages/Home/Home.jsx';
 import Info from './pages/Info/Info.jsx';
 import Games from './pages/Games/Games.jsx';
 import Dictionary from './pages/Dictionary/Dictionary.jsx';
 
-function App() {
+function Layout({ children }) {
   return (
-    <BrowserRouter basename="/TCU-625">
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/informacion" element={<Info />} />
-        <Route path="/juegos" element={<Games />} />
-        <Route path="/diccionario" element={<Dictionary />} />
-      </Routes>
-    </BrowserRouter>
+      {children}
+    </>
   );
+}
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout><Home /></Layout>,
+    },
+    {
+      path: '/informacion',
+      element: <Layout><Info /></Layout>,
+    },
+    {
+      path: '/juegos',
+      element: <Layout><Games /></Layout>,
+    },
+    {
+      path: '/diccionario',
+      element: <Layout><Dictionary /></Layout>,
+    },
+  ],
+  { basename: '/TCU-625' }
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
