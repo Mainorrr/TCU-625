0d6dff173c79192a2fa76dc7a6725dbfbb37a180
Mainorrr <mainor.cas.v@gmail.com>
2026-05-17 14:13:13 -0600

    Usar HashRouter para rutas confiables en GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,22 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Home from './pages/Home/Home.jsx';
 import Info from './pages/Info/Info.jsx';
 import Games from './pages/Games/Games.jsx';
 import Dictionary from './pages/Dictionary/Dictionary.jsx';
 
-function Layout({ children }) {
-  return (
-    <>
-      <Header />
-      {children}
-    </>
-  );
-}
-
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Layout><Home /></Layout>,
-    },
-    {
-      path: '/informacion',
-      element: <Layout><Info /></Layout>,
-    },
-    {
-      path: '/juegos',
-      element: <Layout><Games /></Layout>,
-    },
-    {
-      path: '/diccionario',
-      element: <Layout><Dictionary /></Layout>,
-    },
-  ],
-  { basename: '/TCU-625' }
-);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/informacion" element={<Info />} />
+        <Route path="/juegos" element={<Games />} />
+        <Route path="/diccionario" element={<Dictionary />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
